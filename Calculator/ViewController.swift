13b783aad6e2d61bd47e80fe1560b54155aea199
Ihashi <ihashi91@yahoo.fr>
2015-04-07 13:12:58 +0200

    Racine carré + positions boutons

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -43,6 +43,7 @@ class ViewController: UIViewController
         case"÷": performOperation { $1 / $0 }
         case"+": performOperation { $0 + $1 }
         case"−": performOperation { $1 - $0 }
+        case"√": performOperation { sqrt($0) }
         default: break
         }
     }
@@ -52,6 +53,15 @@ class ViewController: UIViewController
         if operandStack.count >= 2
         {
             displayValue = operation(operandStack.removeLast(), operandStack.removeLast())
+            enter()
+        }
+    }
+    
+    func performOperation(operation: Double -> Double)
+    {
+        if operandStack.count >= 1
+        {
+            displayValue = operation(operandStack.removeLast())
             enter()
         }
     }
